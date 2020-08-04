75f4077dc2870de1c2ea47024dff4dd806772224
Taynara Sene <tpereira@traylabs.com.br>
2020-08-04 20:44:30 -0300

    implementa a criação de aulas

--- server/src/database/migrations/03_create_connections.ts
+++ server/src/database/migrations/03_create_connections.ts
@@ -4,15 +4,16 @@ export async function up(knex: Knex){
     return knex.schema.createTable('connections', table =>{
         table.increments('id').primary();
 
-      
-
-
         table.integer('user_id')
             .notNullable()
             .references('id')
             .inTable('users')
             .onUpdate('CASCADE')
             .onDelete('CASCADE');
+
+        table.timestamp('created_at')
+        .defaultTo('now()')
+        .notNullable();
     })
 }
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/users', (request, response)=>{
-    const users = [
-        {name: 'Taynara', age: 25},
-        {name: 'Felipe', age: 25},
-
-    ]
-    return response.json(users);
-})
+app.use(routes);
 
 
 app.listen(3333);
