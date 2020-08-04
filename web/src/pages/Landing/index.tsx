5aea89b4352c0b1ecb6a61a2820ebf3dd8ad5bf2
Taynara Sene <tpereira@traylabs.com.br>
2020-08-04 16:21:12 -0300

    adiciona rotas

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import { Link } from 'react-router-dom';
+
 import LogoImg from '../../assets/images/logo.svg';
 import LandingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
@@ -22,15 +25,15 @@ function Landing(){
                 />
 
                 <div className="buttons-container">
-                    <a href="#" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar"/>
                         Estudar
-                    </a>
+                    </Link>
 
-                    <a href="#" className="give-classes">
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Dar aulas"/>
                         Dar aulas
-                    </a>  
+                    </Link>  
                 </div>
                 <span className="total-connections">
                         Total de 200 conexões já realizadas <img src={ purpleHeartIcon} alt="coração roxo"/>
