3526fbc6370f3a4ea3dd3a65f327e13a0712c160
Taynara Sene <tpereira@traylabs.com.br>
2020-08-04 22:57:28 -0300

    implementa connections

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConectionsController from './controllers/ConectionsController';
 
 
 const routes = express.Router();
 const ClassesControllers = new ClassesController();
+const connectionsContrololers = new ConectionsController();
 
 routes.get('/classes', ClassesControllers.index);
 routes.post('/classes', ClassesControllers.create);
-
+routes.get('/connections', connectionsContrololers.index);
+routes.post('/connections', connectionsContrololers.create);
 
 export default routes;
